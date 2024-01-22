1f7138a4890ad79620b3865c2a2e96d476c34ea6
nck4s <nn.2nn@mail.ru>
2024-01-23 01:10:12 +1000

    second commit

--- main/models.py
+++ main/models.py
@@ -4,3 +4,6 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
